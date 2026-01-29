dd03d2ed73be1540390812beedea6fd235fe199c
nakul <jaglan.nakul@gmail.com>
2026-01-29 20:56:23 +0530

    removed 2 sections

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <SliderSection />
-      <WhyChooseUsSection />
-      <TestimonialsSection />
+      {/* <WhyChooseUsSection /> */}
+      {/* <TestimonialsSection /> */}
     </>
   );
 }
